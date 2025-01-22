0111c158c4dcb57bbfecd495d485a135e27089c8
Valentin Hamon <pro.valentin.hamon@gmail.com>
2025-01-22 16:14:33 +0100

    ajout icon statut

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,8 +1,23 @@
 import { motion, AnimatePresence } from "framer-motion";
 import PropTypes from "prop-types";
 import ReactStars from "react-rating-stars-component";
+import { FaBook, FaBookOpen, FaCheck } from "react-icons/fa";
 
 function BookList({ books, deleteBook, onUpdateStatus, onUpdateRating }) {
+
+  const getStatusIcon = (status) => {
+    switch (status) {
+      case "À lire":
+        return <FaBook className="text-yellow-500" />;
+      case "En cours":
+        return <FaBookOpen className="text-blue-500" />;
+      case "Lu":
+        return <FaCheck className="text-green-500" />;
+      default:
+        return null;
+    }
+  };
+
   return (
     <div className="p-4">
       <h2 className="text-2xl font-bold mb-4">Ma Liste de Livres</h2>
@@ -16,8 +31,14 @@ function BookList({ books, deleteBook, onUpdateStatus, onUpdateRating }) {
                 animate={{ opacity: 1, y: 0 }}
                 exit={{ opacity: 0, y: 20 }}
                 transition={{ duration: 0.3 }}
-                className="bg-white shadow-md rounded-md p-4 flex flex-col items-center dark:bg-gray-800"
+                className="relative bg-white shadow-md rounded-md p-4 flex flex-col items-center dark:bg-gray-800"
               >
+                <button
+                  onClick={() => onUpdateStatus(book._id || book.id)}
+                  className="absolute top-2 left-2 p-2 rounded-full bg-gray-200 dark:bg-gray-700"
+                >
+                  {getStatusIcon(book.status)}
+                </button>
                 <img
                   src={book.cover}
                   alt={book.title}
@@ -25,14 +46,7 @@ function BookList({ books, deleteBook, onUpdateStatus, onUpdateRating }) {
                 />
                 <h3 className="text-lg font-semibold dark:text-white">{book.title}</h3>
                 <p className="text-gray-500 dark:text-white">Auteur : {book.author}</p>
-                <p className="text-gray-500 dark:text-white">Statut : {book.status}</p>
                 <div className="flex space-x-2 mt-4">
-                  <button
-                    onClick={() => onUpdateStatus(book._id || book.id)}
-                    className="bg-yellow-500 text-white px-4 py-2 rounded"
-                  >
-                    Changer Statut
-                  </button>
                   <button
                     onClick={() => deleteBook(book._id || book.id)}
                     className="bg-red-500 text-white px-4 py-2 rounded"
@@ -79,4 +93,3 @@ BookList.propTypes = {
 };
 
 export default BookList;
-
